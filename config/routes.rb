3e497aa105d9d959d5e7bb82e5a0e8ac8accbea7
Susana Ramallo <ramallo.susana@gmail.com>
2013-02-26 17:14:16 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 SampleApp::Application.routes.draw do
+ 
+  #get "static_pages/..."
+
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
